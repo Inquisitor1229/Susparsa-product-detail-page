b0c39dba5f5dd350f862835c264b0435c7e6cbbb
Inquisitor1229 <36363019+Inquisitor1229@users.noreply.github.com>
2022-05-17 03:02:22 +0300

    add js for accordion and fix accordion border

---
+++ js/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+class Accordion {
+    constructor(domNode) {
+        this.rootEl = domNode;
+        this.buttonEl = this.rootEl.querySelector('button[aria-expanded]');
+
+        const controlsId = this.buttonEl.getAttribute('aria-controls');
+        this.contentEl = document.getElementById(controlsId);
+
+        this.open = this.buttonEl.getAttribute('aria-expanded') === 'true';
+
+        this.buttonEl.addEventListener('click', this.onButtonClick.bind(this));
+    }
+
+    onButtonClick() {
+        this.toggle(!this.open);
+    }
+
+    toggle(open) {
+        if (open === this.open) {
+            return;
+        }
+
+        this.open = open;
+
+        this.buttonEl.setAttribute('aria-expanded', `${open}`);
+        if (open) {
+            this.contentEl.removeAttribute('hidden');
+        } else {
+            this.contentEl.setAttribute('hidden', '');
+        }
+    }
+
+    open() {
+        this.toggle(true);
+    }
+
+    close() {
+        this.toggle(false);
+    }
+}
+
+const accordions = document.querySelectorAll('.accordion h3');
+accordions.forEach((accordionEl) => {
+    new Accordion(accordionEl);
+});
